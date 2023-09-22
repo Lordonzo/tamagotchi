544ba27a81b4c6c8ddabed2e921ab389d49862b8
Lordonzo <vthevenon29@outlook.fr>
2023-09-22 16:18:58 +0200

    db

--- models/database/DAOContext.java
+++ models/database/DAOContext.java
@@ -10,19 +10,19 @@ public class DAOContext {
     protected static String dbURL;
     protected static String dbLogin;
     protected static String dbPassword;
-    protected Connection connection;
 
-    public void LoadConnection() {
+    public Connection LoadConnection() {
         dbURL = "jdbc:sqlite:sqlite"+File.separator+"db"+File.separator+"tamagotchi.db";
         try {
             Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection(dbURL, "root", "");
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            return DriverManager.getConnection(dbURL, "root", "");
         } catch (ClassNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
+        return null;
     }
 }
